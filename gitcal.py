31d2d23717df8e2d759eb3e693391ad3507c4cb1
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-03-18 20:18:44 -0400

    add basic draw_tables()

--- gitcal.py
+++ gitcal.py
@@ -9,14 +9,13 @@ class Table:
         self.data = data
 
     def draw_table(self):
-        row_count = len(self.data)
         table = ""
         idx = 0
         row = 1
 
         for result in self.draw_table_iter():
             idx += 1
-            if idx == self.cell_info.height and row < row_count:
+            if idx == self.cell_info.height and row < self.row_count():
                 row += 1
                 idx = 0
                 if self.cell_info.has_border:
@@ -53,6 +52,63 @@ class Table:
     def draw_cell_iter(self, val):
         for res in self.cell_info.fnc_draw_cell(self.cell_info.fnc_getval(val)):
             yield res
+
+    def row_count(self):
+        return len(self.data)
+
+    @staticmethod
+    def draw_tables(tablelist, **kwargs):
+        spacing = kwargs.get('spacing', 2)
+        max_lengths = [ 0 ] * len(tablelist)
+        row_counter = [ 0 ] * len(tablelist)
+        lne_counter = [ 0 ] * len(tablelist)
+        result = ''
+        last_result_len = 0
+
+        gen_list = [ tbl.draw_table_iter() for tbl in tablelist ]
+        gen_done = 0
+
+        while gen_done != len(gen_list):
+            draws_done = 0
+
+            for gidx in range(len(gen_list)): # pylint: disable=consider-using-enumerate
+                gen = gen_list[gidx]
+                tbl = tablelist[gidx]
+                do_draw = True
+
+                try:
+                    res = next(gen)
+                    if len(res) > max_lengths[gidx]:
+                        max_lengths[gidx] = len(res)
+
+                    lne_counter[gidx] += 1
+                    if lne_counter[gidx] == tbl.cell_info.height:
+                        row_counter[gidx] += 1
+                        lne_counter[gidx] = 0
+
+                        if tbl.cell_info.has_border and tbl.row_count() != row_counter[gidx]:
+                            do_draw = False
+                except StopIteration:
+                    do_draw = False
+                    gen_done += 1
+
+                if do_draw:
+                    result += res
+                    draws_done += 1
+                else:
+                    if gidx != len(gen_list) - 1:
+                        result += ' ' * max_lengths[gidx]
+
+                if gidx != len(gen_list) - 1:
+                    result += ' ' * spacing
+
+            if draws_done == 0:
+                result = result[:last_result_len]
+            else:
+                result += '\n'
+            last_result_len = len(result)
+
+        return result
 
 class CellInfo:
     def __init__(self, **kwargs):
@@ -93,13 +149,23 @@ def main():
         getval=getval
     )
 
-    tbl = Table(cell_unborder)
+    tbl = Table(cell_bordered)
     tbl.set_table_data([
         [1, 0, 1, 1],
         [1, 1, 0, 0],
         [1, 0, 1, 0]
     ])
-    print(tbl.draw_table(), end='')
+
+    tbl2 = Table(cell_bordered)
+    tbl2.set_table_data([
+        [0, 1, 1, 1],
+        [0, 1, 0, 0],
+        [0, 1, 1, 1]
+    ])
+
+    print(tbl.draw_table())
+
+    print(Table.draw_tables( (tbl, tbl2) ))
 
 if __name__ == '__main__':
     main()
