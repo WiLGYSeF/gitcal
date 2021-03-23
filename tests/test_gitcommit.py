b7853c1bc9d91229e87a98fbabeb90738f745c65
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-03-23 10:28:44 -0400

    add tests for shortdate()

--- tests/test_gitcommit.py
+++ tests/test_gitcommit.py
@@ -19,7 +19,37 @@ SHORTDATE = [
         DELTA: datetime.timedelta(days=1),
         INCLUDE_YEAR: True,
         RESULT: '2021-03-23'
-    }
+    },
+    {
+        DTIME: mkdtime('2021-03-23 10:24:12'),
+        DELTA: datetime.timedelta(days=1),
+        INCLUDE_YEAR: False,
+        RESULT: '03-23'
+    },
+    {
+        DTIME: mkdtime('2021-03-23 10:24:12'),
+        DELTA: datetime.timedelta(hours=4),
+        INCLUDE_YEAR: True,
+        RESULT: '2021-03-23 10h'
+    },
+    {
+        DTIME: mkdtime('2021-03-23 10:24:12'),
+        DELTA: datetime.timedelta(hours=4),
+        INCLUDE_YEAR: False,
+        RESULT: '03-23 10h'
+    },
+    {
+        DTIME: mkdtime('2021-03-23 10:24:12'),
+        DELTA: datetime.timedelta(hours=4, minutes=18),
+        INCLUDE_YEAR: True,
+        RESULT: '2021-03-23 10:24:12'
+    },
+    {
+        DTIME: mkdtime('2021-03-23 10:24:12'),
+        DELTA: datetime.timedelta(hours=4, minutes=18),
+        INCLUDE_YEAR: False,
+        RESULT: '2021-03-23 10:24:12'
+    },
 ]
 
 
